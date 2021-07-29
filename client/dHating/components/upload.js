7f2183bb663ebbb1e963614daf30c7279b130c03
Arturzadas <arturmfernandes96@gmail.com>
2021-07-29 12:42:03 +0100

    added image picker, working on image upload

--- client/dHating/components/upload.js
+++ client/dHating/components/upload.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
+export default function Upload() {
 
-function Upload({ route, navigation }) {
+  const [image, setImage] = useState(null);
 
-  const { user } = route.params;
-  console.log(user);
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
 
-  console.log('upload files')
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
-    <View>
-      <Text>{user.firstName}</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
-  )
+  );
 }
-
-export default Upload
