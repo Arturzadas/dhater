1eda97521b838dc720649d435a8687c5cb298e5e
Arturzadas <arturmfernandes96@gmail.com>
2021-07-30 12:49:43 +0100

    installed swiperflatlist and got it working with the dashboard, added some logic to get likes and users, not complete

--- client/dHating/components/dashboard.js
+++ client/dHating/components/dashboard.js
@@ -1,15 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, Pressable, Text } from 'react-native';
+import { Dimensions, StyleSheet, View, Platform, Pressable, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import styles from '../styles/styles'
+// import styles from '../styles/styles'
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
+
 
 export default function Upload({ route, navigation }) {
 
   const { user } = route.params;
 
   const [dashUser, setDashUser] = useState({});
+
+  const [people, setPeople] = useState([]);
 
   const api = 'http://localhost:3080'
 
@@ -38,11 +42,32 @@ export default function Upload({ route, navigation }) {
     fetchUserUpdates();
   }, [])
 
+  const colors = [dashUser.firstName, dashUser.lastName, 'skyblue'];
+
+  const { width } = Dimensions.get('window');
+  const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: 'white' },
+    child: { width, justifyContent: 'center' },
+    text: { fontSize: width * 0.1, textAlign: 'center' },
+  });
 
   return (
-    <View style={styles.view}>
-      <Image source={{ uri: dashUser.imgsrc }} style={styles.profileimg} />
-      <Text>Hey {dashUser.firstName}, time to start swiping!</Text>
+    // <View style={styles.view}>
+    //   <Image source={{ uri: dashUser.imgsrc }} style={styles.profileimg} />
+    //   <Text>Hey {dashUser.firstName}, time to start swiping!</Text>
+    // </View>
+    <View style={styles.container}>
+      <SwiperFlatList index={0} showPagination>
+        <View style={styles.child}>
+          <Text>1st child</Text>
+        </View>
+        <View style={styles.child}>
+          <Text>2nd child</Text>
+        </View>
+        <View style={styles.child}>
+          <Text>3rd child</Text>
+        </View>
+      </SwiperFlatList>
     </View>
   )
 }
