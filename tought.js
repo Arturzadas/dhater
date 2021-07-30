7691f39242404a41f108ba35d2ee360413ca4596
Arturzadas <arturmfernandes96@gmail.com>
2021-07-30 21:55:09 +0100

    check back tomorrow

--- tought.js
+++ tought.js
@@ -1,20 +1,22 @@
 // TODO
 
-//! upload or login
-//?   configure routing buttons and titles
+//?   upload or login ---- upload is done but login could use more security features
+//?   configure routing buttons and titles ---- need more stylesheet and logos etc
 //*   check out gestures for swiping in the app
 //   dashboard:
 //*     3 screens - dislikes, people, matches
 //     dislikes to dislike new things
-//     people to match new people
+//?     people to match new people ---- still need to add more user details + stylesheet
 //     matches w/ chat interface
 
-//     each person should show what dislikes you have in common
+//!     each person should show what dislikes you have in common
 
 
 //*   each user should have a 'progress' number associated to it, so we can know if he's uploaded the image and answered the dislike questions
 //?   start styling the app properly!
 
+//* ADD MATCHES TO USER !!!!
+//* UPDATE DASHUSER WITH THE NEW MATCHES!!!!!
 
 
 
@@ -29,6 +31,3 @@
 
 
 
-
-//! ADD MATCHES TO USER !!!!
-//! UPDATE DASHUSER WITH THE NEW MATCHES!!!!!
