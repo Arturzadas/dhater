a23428cb6c8df85b4ec28daaf4afa4dc4274a8dd
Arturzadas <arturmfernandes96@gmail.com>
2021-07-31 14:19:54 +0100

    added basic functionality to textinput, setting currentMessage state and 2-way data binding

--- client/dHating/components/chat.js
+++ client/dHating/components/chat.js
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,9 +11,15 @@ function Chat ({ navigation, route }) {
   const { matched } = route.params;
   const { chat } = route.params;
 
+
+  const [currentMessage, setCurrentMessage] = useState('');
   // console.log(user, matched, chat)
 
+  const handleChange = (value) => {
+    setCurrentMessage(value);
+  };
 
+  console.log(currentMessage);
 
 
   return (
@@ -29,9 +35,12 @@ function Chat ({ navigation, route }) {
         <TextInput
         style={styles.messageInput}
         name="message"
+        value={currentMessage}
+        onChangeText={(text) => handleChange(text)}
         ></TextInput>
         <Pressable 
         style={styles.sendBtn}
+        onPress={()=> handleChange('')}
         >
         <Text
         style={styles.sendText}
